1fc79d319460487230f1f9d7dda196015a9d5dda
zhangxuqiang <zhangxuqiang@ccc.net>
2020-08-10 19:02:18 +0800

    修改ID设置

--- update_UI.py
+++ update_UI.py
@@ -1,11 +1,41 @@
+# coding:utf-8
 import os
 import pprint
 import glob
+import re
+
+
+# defaultRoot = "Z:\\"
+
+
+def getAssetRoot(drive, project):
+    return os.path.join(drive, project, "assets")
 
 
 def get_assets(asset_root, charType):
     path = os.path.join(asset_root, charType)
-    return os.listdir(path)
+    if os.path.isdir(path):
+        return os.listdir(path)
+    else:
+        return []
+
+
+def get_asset_type(asset_name):
+    asset_re = r"([a-zA-Z])\d+[a-zA-Z]+"
+    matched = re.match(asset_re, asset_name)
+    mapping = {
+        "s": "set",
+        "c": "char",
+        "p": "prop",
+        "e": "elem",
+
+    }
+    if matched:
+        asset_type_char = matched.group(1)
+        asset_type = mapping.get(asset_type_char, "null")
+    else:
+        asset_type = "null"
+    return asset_type
 
 
 def get_varient(asset_root, asset_type, asset, varient_type):
@@ -21,19 +51,28 @@ def get_varient(asset_root, asset_type, asset, varient_type):
 
 
 def asset_id_update(eventType, eventId, param, node):
-    print eventType,eventType
+    print eventType, eventType
     # monitor asset id
     if node.getType() != "Group":
         return
     typeParm = node.getParameter("user.type")
     if typeParm:
-        print("type")
+        # print("type")
         typeValue = typeParm.getValue(0)
         if typeValue == "AssetLoader":
-            print("AssetLoader")
-            if param.getFullName().endswith("user.ID"):
-                print("user.ID")
-                asset_root = node.getParameter("user.asset_root").getValue(0)
+            # print("AssetLoader")
+            drive = node.getParameter("user.root").getValue(0)
+            project = node.getParameter("user.project").getValue(0)
+            if param.getFullName().endswith("user.ID") or param.getFullName().endswith("user.project"):
+                if param.getFullName().endswith("user.ID"):
+                    asset = node.getParameter("user.ID").getValue(0)
+                    # rename node
+                    node.setName(asset)
+                    # change asset type
+                    asset_type_name = get_asset_type(asset)
+                    node.getParameter("user.assetType").setValue(asset_type_name, 0)
+
+                asset_root = getAssetRoot(drive, project)
                 asset_type = node.getParameter("user.assetType").getValue(0)
                 asset = node.getParameter("user.ID").getValue(0)
                 geo_varient = get_varient(asset_root, asset_type, asset, "geo")
@@ -42,16 +81,6 @@ def asset_id_update(eventType, eventId, param, node):
                 node.getParameter("user.geo_varient").setHintString(pprint.pformat(hint))
                 hint = {'widget': 'popup', 'options': look_varient}
                 node.getParameter("user.look_varient").setHintString(pprint.pformat(hint))
-                Utils.EventModule.ProcessAllEvents()
-            if param.getFullName().endswith("user.assetType"):
-                print("user.assetType")
-                asset_root = node.getParameter("user.asset_root").getValue(0)
-                asset_type = node.getParameter("user.assetType").getValue(0)
-                assets = get_assets(asset_root, asset_type)
-                hint = {'widget': 'popup', 'options': assets}
-                node.getParameter("user.ID").setHintString(pprint.pformat(hint))
-                
-
 
     else:
         return
@@ -61,7 +90,7 @@ eventType = "parameter_finalizeValue"
 # eventId = "asset_id"
 
 try:
-    Utils.EventModule.RegisterEventHandler(asset_id_update, eventType=eventType,)
+    Utils.EventModule.RegisterEventHandler(asset_id_update, eventType=eventType, )
 except:
     print "error"
     # Utils.EventModule.UnregisterEventHandler(asset_id_update, eventType=eventType, eventID=eventId, )
